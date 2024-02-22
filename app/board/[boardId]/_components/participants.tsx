53a4e1dc2706723f2a3502d0e1331219eac5c120
Haqi Ramadhani <pengusahasukses1999@gmail.com>
2024-02-22 12:19:32 +0700

    Add room online participant on canvas

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,9 +1,44 @@
+"use client";
+
 import { Skeleton } from "@/components/ui/skeleton";
+import { useOthers, useSelf } from "@/liveblocks.config";
+import { UserAvatar } from "./user-avatar";
+import { connectionIdColor } from "@/lib/utils";
+
+const MAX_SHOWN_USERS = 2;
 
 export const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const numberOfMoreUsers = users.length - MAX_SHOWN_USERS;
+
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-      List of users
+      <div className="flex gap-x-2">
+        {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => (
+          <UserAvatar
+            borderColor={connectionIdColor(connectionId)}
+            key={connectionId}
+            src={info?.picture}
+            name={info?.name}
+            fallback={info?.name?.[0] || "T"}
+          />
+        ))}
+        {currentUser && (
+          <UserAvatar
+            borderColor={connectionIdColor(currentUser.connectionId)}
+            src={currentUser.info?.picture}
+            name={`${currentUser.info?.name} (You)`}
+            fallback={currentUser.info?.name?.[0]}
+          />
+        )}
+        {numberOfMoreUsers > 0 && (
+          <UserAvatar
+            name={`${numberOfMoreUsers} more`}
+            fallback={`+${numberOfMoreUsers}`}
+          />
+        )}
+      </div>
     </div>
   );
 };
