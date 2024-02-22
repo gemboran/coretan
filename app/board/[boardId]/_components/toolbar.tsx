2b5c58f76827e38e8f4154e9683f780aff5b4248
Haqi Ramadhani <pengusahasukses1999@gmail.com>
2024-02-22 07:15:47 +0700

    Setup liveblocks

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,3 +1,5 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export const Toolbar = () => {
   return (
     <div className="absolute top-1/2 -translate-y-1/2 left-2 flex flex-col gap-y-4">
@@ -12,6 +14,14 @@ export const Toolbar = () => {
         <div>Undo</div>
         <div>Redo</div>
       </div>
+    </div>
+  );
+};
+
+Toolbar.Skeleton = function ToolbarSkeleton() {
+  return (
+    <div className="absolute top-1/2 -translate-y-1/2 left-2 flex flex-col gap-y-4 bg-white shadow-md rounded-md h-[360px] w-[52px]">
+      <Skeleton className="h-full w-full bg-white" />
     </div>
   );
 };
